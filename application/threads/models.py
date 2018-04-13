71b6d708d1ea5deb503e079f7356637513763780
Mirex97 <Mirex97@gmail.com>
2018-04-13 20:09:43 +0300

    Postgre testaus

--- application/threads/models.py
+++ application/threads/models.py
@@ -31,12 +31,11 @@ class Thread(db.Model):
 
     @staticmethod
     def find_my_threads(account_id):
-        stmt = text("SELECT Thread.id, Thread.name, Section.name FROM Thread"
-                    " LEFT JOIN Section ON Section.id = Thread.section_id"
-                    " WHERE (Thread.account_id IS :accountid)"
-                    " GROUP BY Thread.id"
-                    " ORDER BY Thread.date_created"
-                    " DESC").params(accountid=account_id)
+        #wat
+        stmt = text("SELECT  Thread.id, Thread.name, Account.username FROM Thread"
+                    " LEFT JOIN Account ON Account.id = Thread.account_id"
+                    " LEFT JOIN Section ON Section.id = Thread.section_id WHERE (Section.ID = :accountid)"
+                    " GROUP BY Thread.id ORDER BY Thread.date_created DESC").params(accountid=account_id)
         res = db.engine.execute(stmt)
         response = []
         for row in res:
